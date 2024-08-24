7978df0c5b4c2af551751fb4adfc481b31acfae1
EL Alami Aymane <elalamiaymane97@gmail.com>
2024-08-24 16:26:31 +0100

    init commit with firebase config

---
+++ android/app/src/main/kotlin/com/example/myvitals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myvitals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:myvitals/screens/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
